b95b543893779016afe600cf780081d021632be1
shyam9741405520 <64199515+shyam9741405520@users.noreply.github.com>
2020-05-13 11:55:35 +0530

    Add files via upload

---
+++ Glass.js
@@ -0,0 +1,10 @@
+class Glass{
+    constructor(x,y,width,height){
+    var options ={
+      restitution:0.5
+          }
+this.body=Bodies.rectangle(200,100,10,200)
+
+
+    }
+}
---
+++ sketch.js
@@ -0,0 +1,17 @@
+var stick1,stick2;
+var stick3,stick4;
+var stick5,stick6;
+var stick7,stick8;
+var ground;
+
+
+
+function setup() {
+  createCanvas(800,400);
+  createSprite(400, 200, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+}
